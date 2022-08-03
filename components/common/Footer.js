816127978cc2ac27de5d66dde2ab1ee1ad114b93
hh <hhlee@cucon.kr>
2022-08-03 16:10:09 +0900

    test

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer>
             <p className="copyright">
-                COPYRIGHT(C) 2022 CHAEUM CNI Inc., ALL RIGHTS RESERVED.
+                COPYRIGHT(C) 2022 Inc., ALL RIGHTS RESERVED.
             </p>
         </footer>
     );
